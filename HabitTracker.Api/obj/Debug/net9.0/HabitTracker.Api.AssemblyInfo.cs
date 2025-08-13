d4f9a6421334d9200b9dcaf65c4e65bbd4ed502a
Kevin <kevinjoelrobles@gmail.com>
2025-08-12 21:52:13 -0400

    Saving

--- HabitTracker.Api/obj/Debug/net9.0/HabitTracker.Api.AssemblyInfo.cs
+++ HabitTracker.Api/obj/Debug/net9.0/HabitTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48bc7699daec5d2e37e5bee8fc84bdd2043f678")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
